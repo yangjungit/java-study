b24a93fd0275b1ea66105527964c1d8d878c3fdc
yangjun <1102934697@qq.com>
2020-04-24 14:03:02 +0800

    fix

--- src/main/resources/databases/uuu.sql
+++ src/main/resources/databases/uuu.sql
@@ -1,15 +1,7 @@
 /*
- Navicat Premium Data Transfer
 
- Source Server         : local
  Source Server Type    : MySQL
- Source Server Version : 50728
- Source Host           : localhost:3306
  Source Schema         : test
-
- Target Server Type    : MySQL
- Target Server Version : 50728
- File Encoding         : 65001
 
  Date: 14/04/2020 17:33:06
 */
